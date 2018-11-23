614a86c8022044ebb88b8fe2d865948eba7809b0
Ace <ace@mdev.digital>
2018-11-23 13:33:31 -0500

    Added line break test to typo sheet

--- pages/typography.vue
+++ pages/typography.vue
@@ -2,6 +2,8 @@
   .typography-page
     h1.u-bg-navy
       |h1. Nymphs blitz quick vex dwarf jog.
+    h1.u-navy
+      |h1. Nymphs blitz quick vex dwarf jog.
     h2
       |h2. Nymphs blitz quick vex dwarf jog.
     h3
@@ -24,4 +26,39 @@
     br
     a(href="/typography")
       |a. Nymphs blitz quick vex dwarf jog.
+    // Line Spacing Test
+    br
+    br
+    br
+    span
+      |LINE BREAK TEST:
+    br
+    br
+    br
+    h1.u-bg-navy
+      |h1. Nymphs blitz<br/>quick vex dwarf jog.
+    h1.u-navy
+      |h1. Nymphs blitz<br/>quick vex dwarf jog.
+    h2
+      |h2. Nymphs blitz<br/>quick vex dwarf jog.
+    h3
+      |h3. Nymphs blitz<br/>quick vex dwarf jog.
+    h4
+      |h4. Nymphs blitz<br/>quick vex dwarf jog.
+    h5
+      |h5. Nymphs blitz<br/>quick vex dwarf jog.
+    p
+      |p. Nymphs blitz<br/>quick vex dwarf jog.
+    p.--body-medium
+      |p medium. Nymphs<br/>blitz quick vex dwarf jog.
+    p.--body-large
+      |p large. Nymphs<br/>blitz quick vex dwarf jog.
+    p
+      |p b em. Nymphs <b>blitz quick</b><br/>vex <em>dwarf jog</em>.
+    br
+    a.agcap-btn(href="/typography")
+      |BUTTON LOOKS<br/>LIKE THIS
+    br
+    a(href="/typography")
+      |a. Nymphs blitz<br/>quick vex dwarf jog.
 </template>
