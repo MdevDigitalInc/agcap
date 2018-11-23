b2fae87b57274c6fade2772e377dda079d858f87
Ace <ace@mdev.digital>
2018-11-23 10:55:46 -0500

    Added styleguide information to solution

---
+++ pages/typography.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  .typography-page
+    h1
+      |h1. Nymphs blitz quick vex dwarf jog.
+    h2
+      |h2. Nymphs blitz quick vex dwarf jog.
+    h3
+      |h3. Nymphs blitz quick vex dwarf jog.
+    h4
+      |h4. Nymphs blitz quick vex dwarf jog.
+    h5
+      |h5. Nymphs blitz quick vex dwarf jog.
+    p
+      |p. Nymphs blitz quick vex dwarf jog.
+    p.--body-medium
+      |p. Nymphs blitz quick vex dwarf jog.
+    p.--body-large
+      |p. Nymphs blitz quick vex dwarf jog.
+    p
+      |p. Nymphs <b>blitz quick</b> vex <em>dwarf jog</em>.
+    a
+      |a. Nymphs blitz quick vex dwarf jog.
+    br
+    a.agcap-btn
+      |BUTTON LOOKS LIKE THIS
+</template>
