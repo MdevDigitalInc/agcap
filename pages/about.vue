acd033e446bf4a11b45f5833845f8c5262ba98e2
Ace <ace@mdev.digital>
2018-11-26 11:55:39 -0500

    WIP commit, introduction section in progress

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,59 @@
 <template lang="pug">
   .agc-about-page
-    h1.u-navy
-      |Introduction
+    // About Introduction Section
+    section.agc-about-intro
+      // Left Side
+      .agc-intro-text
+        h5
+          |who we are
+        h1.u-navy
+          |Introduction
+        p
+          |Ag Capital Canada involves two entities:
+        ul
+          li
+            |A General Partnership (GP) consisting of five managing principals. These principals are Jay Bradshaw, John DePutter, John Lansink, Michael Perovich and a fifth, as-yet-to-be announced individual who will reside (and have experience) in Western Canada.
+          li
+            |A Limited Partnership (LP) consisting of investors. All of the five General Partners are also investors. A total of no more than 50 investors will constitute the Limited Partnership.
+        p
+          |The spirit of the managing principals and the investors will involve interaction with a group of like-minded individuals.
+        .agc-intro-vision-alpha
+        h2
+          |Our vision
+        .agcap-title-line
+        p
+          |We are forming a group of investors who can offer advice and expertise to the managing principals and may on occasion, offer services for the companies we decide to invest in.
+        p
+          |Although the managing principals will lead the research into prospective investments and make final decisions as to which companies to invest in, these leaders will consult with and at times ask for input from the investors.
+        p
+          |The approach of Ag Capital Canada is largely about choosing the right people to work with: the mix of people acting as principals, and those in the investment group, must be people with integrity and experience.
+        p
+          |In terms of the owner-operators who apply for our capital and assistance, we are looking for smart, honest, hard-working business owners with bright prospects.
+        a.agcap-btn(href="/typography" tilte="Request investor info package")
+          |Request investor info package
+          i.fas.fa-caret-right
+      // Right Side
+      .agc-intro-content
+      // Secret Introduction Content for Small Screens ONLY!
+      .agc-intro-vision-omega
+        h2
+          |Our vision
+        .agcap-title-line
+        p
+          |We are forming a group of investors who can offer advice and expertise to the managing principals and may on occasion, offer services for the companies we decide to invest in.
+        p
+          |Although the managing principals will lead the research into prospective investments and make final decisions as to which companies to invest in, these leaders will consult with and at times ask for input from the investors.
+        p
+          |The approach of Ag Capital Canada is largely about choosing the right people to work with: the mix of people acting as principals, and those in the investment group, must be people with integrity and experience.
+        p
+          |In terms of the owner-operators who apply for our capital and assistance, we are looking for smart, honest, hard-working business owners with bright prospects.
+        a.agcap-btn(href="/typography" tilte="Request investor info package")
+          |Request investor info package
+          i.fas.fa-caret-right
+    // About Banner Image
+    section.agc-about-banner
+    // About Bio Section
+    section.agc-about-bio
+      p.agc-bio-blurb.--body-medium.
+        |Real expertise is built from combined experience. Our team offers a unique mix of industry perspectives and specialties, creating a rare breadth and depth of knowledge. Our General Partner Managers and their specialties include:
 </template>
