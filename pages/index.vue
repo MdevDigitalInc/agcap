0afc8b1a35b3c0295cb36f7e7323b1bf0a5da79f
Ace <ace@mdev.digital>
2018-11-26 11:06:42 -0500

    Added about us section, fully responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   .agc-home-page
+    // Home Hero Section
     section.agc-hero
       .agc-hero-text
         h5
@@ -10,6 +11,7 @@
           |Helping great businesses grow.
       img.agc-hero-image(src="http://placekitten.com/g/1440/640" alt="todo")
       .agc-hero-panel
+    // Our Approach Section
     section.agc-home-approach
       .agc-approach-text
         h2
@@ -22,17 +24,18 @@
           i.fas.fa-caret-right
       .agc-approach-image
         img(src="http://placekitten.com/g/1122/450" alt="todo")
-    section.agc-home-approach
-      .agc-approach-text
+    // About Us Section
+    section.agc-home-about
+      .agc-about-image
+        img(src="http://placekitten.com/g/1122/450" alt="todo")
+      .agc-about-text
         h2
-          |Fostering Growth with Private Equity
+          |Who We Are
         .agcap-title-line
         p
-          |We provide capital to help owner-operators take their companies to the next, higher level. We also provide the three other key elements necessary for businesses to succeed: partnership, respect, and true expertise.
-        a.agcap-btn(href="/approach" tilte="See our approach")
-          |our approach
+          |We’re a group of down-to-earth farmers, entrepreneurs and ag-business folks from across Canada. Our team offers a unique mix of industry persepctives and specialties, with shared values of entrepreneurialism, hard work and unwavering ethics.
+        a.agcap-btn(href="/about" tilte="Learn about who we are")
+          |about us
           i.fas.fa-caret-right
-      .agc-approach-image
-        img(src="http://placekitten.com/g/1122/450" alt="todo")
     .agc-home-bg
 </template>
