cb503262950088b0a0c052caa11b969d6e44da84
Ace <ace@mdev.digital>
2018-11-26 13:47:12 -0500

    Intro section added and responsive

--- pages/about.vue
+++ pages/about.vue
@@ -18,22 +18,39 @@
         p
           |The spirit of the managing principals and the investors will involve interaction with a group of like-minded individuals.
         .agc-intro-vision-alpha
-        h2
-          |Our vision
-        .agcap-title-line
-        p
-          |We are forming a group of investors who can offer advice and expertise to the managing principals and may on occasion, offer services for the companies we decide to invest in.
-        p
-          |Although the managing principals will lead the research into prospective investments and make final decisions as to which companies to invest in, these leaders will consult with and at times ask for input from the investors.
-        p
-          |The approach of Ag Capital Canada is largely about choosing the right people to work with: the mix of people acting as principals, and those in the investment group, must be people with integrity and experience.
-        p
-          |In terms of the owner-operators who apply for our capital and assistance, we are looking for smart, honest, hard-working business owners with bright prospects.
-        a.agcap-btn(href="/typography" tilte="Request investor info package")
-          |Request investor info package
-          i.fas.fa-caret-right
+          h2
+            |Our vision
+          .agcap-title-line
+          p
+            |We are forming a group of investors who can offer advice and expertise to the managing principals and may on occasion, offer services for the companies we decide to invest in.
+          p
+            |Although the managing principals will lead the research into prospective investments and make final decisions as to which companies to invest in, these leaders will consult with and at times ask for input from the investors.
+          p
+            |The approach of Ag Capital Canada is largely about choosing the right people to work with: the mix of people acting as principals, and those in the investment group, must be people with integrity and experience.
+          p
+            |In terms of the owner-operators who apply for our capital and assistance, we are looking for smart, honest, hard-working business owners with bright prospects.
+          a.agcap-btn(href="/typography" tilte="Request investor info package")
+            |Request investor info package
+            i.fas.fa-caret-right
       // Right Side
       .agc-intro-content
+        .agc-intro-purpose
+          p.u-white.--body-medium
+            |A key purpose of Ag Capital Canada is to offer investors an opportunity to achieve a potentially strong return in an investment vehicle with the following characteristics:
+          ul
+            li.u-white
+              |A pure-play in agriculture
+            li.u-white
+              |Not correlated with broader stock markets
+            li.u-white
+              |No-publicly listed shares
+            li.u-white
+              |All Canadian
+            li.u-white
+              |A core theme of entrepreneurialism: this applies to the managers, the investors, many of whom are self-made business builders – and the people who run the businesses we will invest in.
+            li.u-white
+              |Cautiously managed and directed by five people with a variety of experience and expertise who are also investors.
+        .agc-img-box
       // Secret Introduction Content for Small Screens ONLY!
       .agc-intro-vision-omega
         h2
