31139d57b5a8ca822c01a88ae9981c2385fefa12
Ace <ace@mdev.digital>
2018-11-26 10:17:25 -0500

    Hero implemented, fully responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template lang="pug">
-  .home-page
-    h1.u-blue
-      |Home todo
+  .agc-home-page
+    section.agc-hero
+      .agc-hero-text
+        h5
+          |welcome to
+        h1
+          |Ag Capital Canada
+        p.--body-large.u-white
+          |Helping great businesses grow.
+      img.agc-hero-image(src="http://placekitten.com/g/1440/640" alt="todo")
+      .agc-hero-panel
+    section.agc-morecontent
+      p
+        |Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ornare mauris eget elit finibus maximus. Suspendisse nisi justo, egestas ac hendrerit nec, placerat eu diam. Nulla facilisi. Proin malesuada pellentesque magna et sodales. Ut pharetra ex eget est vulputate eleifend. Ut hendrerit erat vel magna sagittis efficitur. Pellentesque gravida, felis eget aliquet vestibulum, urna sapien finibus odio, id interdum sem neque id nibh.
+    .agc-home-bg
 </template>
