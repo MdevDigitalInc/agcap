c85ddc96988b22835a7e01552f91a41606dba307
Ace <ace@mdev.digital>
2018-12-04 11:47:14 -0500

    Added About section to Approach page

--- pages/approach.vue
+++ pages/approach.vue
@@ -34,5 +34,26 @@
       .agc-approach-intro-right
         .agc-approach-intro-right-content
     section.agc-approach-about
-      |agc-approach-about todo
+      .agc-approach-dialogue
+        p.--body-large
+          |<b>We provide capital. But that’s not all.</b> We help great businesses grow by providing the three other key elements necessary to succeed.
+        .agc-approach-boxes
+          .agc-approach-box
+            h3
+              |Partnership
+            p
+              |True partners aren’t just lenders. They help you identify and work through business roadblocks. They help you develop your ideas and discover new opportunities. They work together with you, in the weeds, to move your business forward. We work with you. Not just for you.
+          .agc-approach-box
+            h3
+              |Respect
+            p
+              |Unlike large investment firms, we know there is not just one roadmap to success. We appreciate the business and culture you’ve created, and the unique ways of doing things that have helped you get to where you are today. It’s your company, your vision.
+          .agc-approach-box
+            h3
+              |True Expertise
+            p
+              |Real expertise is built from combined experience. Our team offers a unique mix of industry perspectives and specialties, creating a rare breadth and depth of knowledge.
+        a.agcap-btn(href="/about" tilte="Learn about who we are")
+          |about us
+          i.fas.fa-caret-right
 </template>
