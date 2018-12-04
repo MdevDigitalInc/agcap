5b8795a7c41aec59c31363717a037065e3d07e36
Ace <ace@mdev.digital>
2018-12-04 13:46:12 -0500

    init

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  footer.agc-footer
+    .agc-footer-main
+      |implement me
+      .agc-footer-main-left
+        .agc-footer-info
+        .agc-footer-contact
+        .agc-footer-links
+      .agc-footer-main-right
+    .agc-footer-bumper
+</template>
