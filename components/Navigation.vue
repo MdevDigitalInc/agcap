6329b4621c48e934ac919d39ff7b1d915859077c
Ace <ace@mdev.digital>
2018-12-06 10:13:14 -0500

    Added basic mobile nav functionality

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,7 +2,7 @@
   .agc-main-nav
     .agc-main-nav-content
       img.agc-main-nav-logo(src="/AGCAP_RGB_Secondary.svg" alt="AGCAP Logo")
-      .agc-main-nav-hamburger
+      .agc-main-nav-hamburger(v-on:click="toggleMobile")
         i.fas.fa-bars
       .agc-main-nav-links
         nuxt-link.agc-main-nav-link.h5(to="/")
@@ -15,3 +15,21 @@
           |Contact
     .agc-main-nav-graphic
 </template>
+
+<script>
+export default {
+  methods: {
+    toggleMobile: function () {
+      var y = document.getElementsByClassName("agc-mobile-nav");
+
+      if (y[0].classList.contains("--hidden")) {
+        y[0].classList.remove("--hidden");
+        document.body.classList.add("u-freeze-scroll");
+      } else {
+        y[0].classList.add("--hidden");
+        document.body.classList.remove("u-freeze-scroll");
+      }
+    }
+  }
+}
+</script>
