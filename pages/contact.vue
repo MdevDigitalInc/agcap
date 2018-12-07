3dcf0416fcdf3af57dde01cf8834635e771b7213
Ace <ace@mdev.digital>
2018-12-07 09:11:54 -0500

    Added everything to contact page except form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,15 @@
 <template lang="pug">
-  .contact-page
-    h1.u-blue
-      |Contact todo
+  .agcap-contact-page
+    section.agcap-contact-content
+      .agcap-contact-content-left
+        h5
+          |CONTACT
+        h1.u-navy
+          |Grow with us
+        p.--body-large
+          |Interested in our services? Request an investor fact sheet.
+        .agcap-contact-form-container
+          |fill me up daddy
+      .agcap-contact-content-right
+        .agcap-contact-image
 </template>
